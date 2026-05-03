e12967ced165489f11b3074beefb0b51198c22c9
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-05-03 08:27:08 +0530

    . database.py

--- database.py
+++ database.py
@@ -22,6 +22,32 @@ async def set_user(uid, data):
         upsert=True
     )
 
+async def add_user(uid):
+    await users.update_one(
+        {"_id": uid},
+        {
+            "$setOnInsert": {
+                "prefix": "",
+                "suffix": "",
+                "caption": "",
+                "thumb": "",
+                "premium": False,
+                "banned": False
+            }
+        },
+        upsert=True
+    )
+
+async def is_banned(uid):
+    user = await get_user(uid)
+    return user.get("banned", False) if user else False
+
+async def is_premium(uid):
+    user = await get_user(uid)
+    return user.get("premium", False) if user else False
+
+async def get_all_users():
+    return users.find({})
 # ------------------------- #
 # Don't Remove Credit 
 # Ask Doubt @AU_Bot_Discussion 
